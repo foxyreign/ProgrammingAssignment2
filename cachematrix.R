f360a0e441c35ccc3032215bf67a67df09c6effd
Adrian Cuyugan <adrian@foxyreign.com>
2015-05-18 05:58:43 +0800

    Renamed matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
-## Write a short comment describing this function
+## Create an inversed matrix calling for another in same environment
+# then starts putting back the values to the new inversed matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  inverse <- NULL
+  set <- function(x) {
+    m <<- x;
+    inverse <<- NULL;
+  }
+  
+  get <- function() return(m);
+  setinv <- function(inv) inverse <<- inv;
+  getinv <- function() return(inverse);
+  return(list(set = set, get = get, 
+              setinv = setinv, 
+              getinv = getinv))
 }
 
+## Computes whatever function is called based on the inversed matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+  inverse <- m$getinv()
+  if(!is.null(inverse)) {
+    message("Getting cached data...")
+    return(inverse)
+  }
+  data <- m$get()
+  invserse <- solve(data, ...)
+  m$setinv(inverse)
+  return(inverse)
 }
